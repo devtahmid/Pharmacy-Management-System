d112c16be46e3265a72b118c5f6f2efd2c0ce70b
khalood17 <106091718+khalood17@users.noreply.github.com>
2022-05-23 17:20:17 +0300

    Update reg_login.php

--- reg_login.php
+++ reg_login.php
@@ -51,40 +51,16 @@
     $h_password=password_hash($password,PASSWORD_DEFAULT);
     $profilepic='default.jpg';
     //$mobile=$country_code.$mobile; will display like that otherwise searching for num becomes difficult in ajax registration
-    $sql="INSERT INTO users(USERNAME,PASSWORD,NAME,CONTACT_NUM,EMAIL,COUNTRY,PROFILE_PIC,BUYER_RATING_SUM,BUYER_RATING_COUNT,SELLER_RATING_SUM,SELLER_RATING_COUNT) VALUES (:uname,:hpwd,:name,:mobile,:email,:country,:pfp,:brs, :brc, :srs, :src)";
-    $db->beginTransaction();
+    $sql="insert into user(UID,Username,Email,Password,Type) values (NULL, '$username', '$password', '$mail', 'Customer')";
+  //  $db->beginTransaction();
     $conn = $db->prepare($sql);
-    $conn->bindValue(':uname' , $username);
-    $conn->bindValue(':hpwd' , $h_password);
-    $conn->bindValue(':name' , $name);
-    $conn->bindValue(':mobile' , $mobile);
-    $conn->bindValue(':email' , $mail);
-    $conn->bindValue(':country' , $country);
-    $conn->bindValue(':pfp' , $profilepic);
-    $conn->bindValue(':brs' , '0');
-    $conn->bindValue(':brc' , '0');
-    $conn->bindValue(':srs' , '0');
-    $conn->bindValue(':src' , '0');
     $conn->execute();
     $Sid= $db->lastInsertId();
-    $sql="INSERT INTO addresses (USER_ID,ADDRESS) VALUES(:sid, :addr1)";
-    $conn = $db->prepare($sql);
-    $conn->bindValue(':sid' , $Sid);
-    $conn->bindValue(':addr1' , $address);
-    $conn->execute();
 
-    $sql="INSERT INTO addresses (USER_ID,ADDRESS) VALUES(:sid, :addr2)";
+    $sql="insert into customer(CID,Fname,Lname,Mobile,Building,Block) values (NULL, '$name', '$lname', '$mobile', '$address', '$block')";
     $conn = $db->prepare($sql);
-    $conn->bindValue(':sid' , $Sid);
-    $conn->bindValue(':addr2' , "");
     $conn->execute();
-
-    $sql="INSERT INTO addresses (USER_ID,ADDRESS) VALUES(:sid, :addr3)";
-    $conn = $db->prepare($sql);
-    $conn->bindValue(':sid' , $Sid);
-    $conn->bindValue(':addr3' , "");
-    $conn->execute();
-    $db->commit();
+  //  $db->commit();
     if ($conn->rowCount()==0) {
     header('location:registration_form.php?error=4');
     }
